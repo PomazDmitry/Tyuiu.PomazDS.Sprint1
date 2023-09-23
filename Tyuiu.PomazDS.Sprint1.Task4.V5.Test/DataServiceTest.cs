3945e88bc87f85b7e7ab1fc17cfa28b68e4d205f
iwsjlt <zed-n.v@mail.ru>
2023-09-23 20:16:52 +0500

    Task4

---
+++ Tyuiu.PomazDS.Sprint1.Task4.V5.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PomazDS.Sprint1.Task4.V5.Lib;
+
+namespace Tyuiu.PomazDS.Sprint1.Task4.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x, y;
+            x = 25;
+            y = 2;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(0.03786, Math.Round(res, 5));
+            
+        }
+    }
+}
---
+++ Tyuiu.PomazDS.Sprint1.Task4.V5/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PomazDS.Sprint1.Task4.V5.Lib;
+
+namespace Tyuiu.PomazDS.Sprint1.Task4.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Помаз Д.С | ИИПб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Class Math                                                        *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Помаз Дмитрий Сергеевич | ИИПб-23-2                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные         *");
+            Console.WriteLine("* данные, вычисляет результат по формуле и печатает его на экране.        *");
+            Console.WriteLine("* Формула: 1 / (|x + √y|)                                                 *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            double x, y;
+
+            Console.WriteLine("Введите значение x:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x, y));
+
+            Console.ReadKey();
+        }
+    }
+}
